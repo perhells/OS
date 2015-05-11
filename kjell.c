2b34dbfd0bd02ac93a37cbcf88bda6212b9bca1e
Per Hellström <perhells@kth.se>
2015-05-11 16:49:24 +0200

    Spinning up the hamster...

--- kjell.c
+++ kjell.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
 #define BUFFERSIZE 100
 
@@ -8,7 +9,7 @@ int exitKjell()
     return 0;
 }
 
-int main()
+int main(int argc, char* argv[], char* envp[])
 {
     char *tok;
     char line[BUFFERSIZE];
@@ -19,7 +20,7 @@ int main()
     printf("%s\n", cwd);
     while(1)
     {   
-        printf("%s",prompt);
+        printf("%s:%s",cwd,prompt);
         fgets(line, sizeof(line), stdin);
         tok = strtok(line, delims);
         while(tok != NULL)
@@ -34,12 +35,65 @@ int main()
                 printf("%s\n", cwd);
                 tok = strtok(NULL, delims);
             }
+            else if(strcmp(tok,"cd")==0)
+            {
+                tok = strtok(NULL, delims);
+                if(chdir(tok)==0)
+                {
+                    getcwd(cwd, sizeof(cwd));
+                    //printf("%s ", tok);
+                }
+            }
+            else if(strcmp(tok,"checkEnv")==0)
+            {
+                printf("0");
+                char *arguments = "";
+                //char *pager = "less";
+
+                //char * new_str ;
+                //if((new_str = malloc(strlen(str1)+strlen(str2)+1)) != NULL){
+                    //new_str[0] = '\0';   // ensures the memory is an empty string
+                    //strcat(new_str,str1);
+                    //strcat(new_str,str2);
+                //} else {
+                    //fprintf(STDERR,"malloc failed!\n");
+                    // exit?
+                //}
+
+
+                while(tok=strtok(NULL,delims))
+                {
+                    printf("1");
+                    //strcat(arguments,tok);   
+                } 
+                if(strcmp(arguments,"")==0)
+                {   
+                    printf("2");
+                    //system("printenv | sort | less");
+                    execlp("printenv", "printenv", NULL);
+                }
+                else
+                {   
+                    //char * derp;
+                    //strcat(derp, "printenv | grep ");
+                    //strcat(derp, arguments);
+                    //strcat(derp, " | sort | pager");
+                    //system(derp);
+                }
+                //system("printenv | grep %s | less", );
+                //while(*envp)
+                //{
+                    //printf("%s\n",*envp);
+                    //envp++;
+                //}
+                tok = strtok(NULL, delims);
+            }
             else
             {
-                printf("%s ", tok);
+                //printf("%s", tok);
                 tok = strtok(NULL, delims);
             }
         }
-        printf("\n");
+        //printf("\n");
     }   
 }
