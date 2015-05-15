48bf39d6882f3eba105e69ffd4ee0b194d7781ed
Per Hellström <perhells@kth.se>
2015-05-15 13:24:32 +0200

    Background processes and signal handling fixed

--- kjell.c
+++ kjell.c
@@ -5,25 +5,46 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <time.h>
-#define BUFFERSIZE 100
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <errno.h>
+#define BUFFERSIZE 80
 #define READ 0
 #define WRITE 1
+#ifndef SIGDET
+#define SIGDET 1
+#endif
 
 FILE *fr;
 
 int exitKjell()
 {
-    return 0;
+    signal(SIGQUIT, SIG_IGN);
+    kill(0, SIGQUIT);
+    exit(0);
 }
 
-void handler(int signum)
+void sandler(int signum)
+{   
+    int pid = waitpid(-1,NULL,WNOHANG);
+    if(pid > 0)
+    {
+        fprintf(stderr,"Process with pid %d has terminated\n",pid);
+    }
+}
+
+void intHandler(int signum)
 {
-    fprintf(stderr,"In handler\n");
+    kill(signum, SIGCHLD);
+    while(waitpid(-1,NULL,WNOHANG)>0){}
 }
 
 int main(int argc, char* argv[], char* envp[])
 {
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+
     char *tok;
     char line[BUFFERSIZE];
     char cwd[BUFFERSIZE];
@@ -34,11 +55,12 @@ int main(int argc, char* argv[], char* envp[])
     char username[BUFFERSIZE];
     char arguments[BUFFERSIZE];
     int status, pipa1[2], pipa2[2], pipa3[2];
+    int foreground;
     char ** res;
     char * p;
     int n_spaces = 0;
     pid_t pid;
-    clock_t start, stop;
+    struct timeval tvalBefore, tvalAfter;
     strcpy(username,getenv("USER"));
     if(getenv("PAGER")!=NULL)
     {
@@ -51,9 +73,16 @@ int main(int argc, char* argv[], char* envp[])
     getcwd(cwd, sizeof(cwd));
     while(1)
     {   
+        if(!SIGDET)
+        {
+            pid = waitpid(-1, &status, WNOHANG);
+            if(pid > 0)
+            {
+                printf("Process with pid %d has terminated.\n", pid);
+            }
+        }
         printf("[%s@%s %s] %s", username, hostname, cwd, prompt);
         fgets(line, sizeof(line), stdin);
-        start = clock();
         tok = strtok(line, delims);
         while(tok != NULL)
         {
@@ -233,36 +262,98 @@ int main(int argc, char* argv[], char* envp[])
                 res = NULL;
                 p = tok;
                 n_spaces = 0; 
+                foreground = 1;
                 
                 while (p) {
-                    res = realloc (res, sizeof (char*) * ++n_spaces);
-                    if (res == NULL)
+                    if(strcmp(p,"&")==0)
                     {
-                        printf("FEL");
+                        p = strtok(NULL,delims);
+                        foreground = 0;
                     }
-                    
-                    res[n_spaces-1] = p;
-                    
-                    p = strtok (NULL, delims);
+                    else
+                    {
+                        res = realloc (res, sizeof (char*) * ++n_spaces);
+                        if (res == NULL)
+                        {
+                            printf("FEL");
+                        }
+                        
+                        res[n_spaces-1] = p;
+                        
+                        p = strtok (NULL, delims);
+                    }
                 }
                 
                 /* realloc one extra element for the last NULL */
                 
                 res = realloc (res, sizeof (char*) * (n_spaces+1));
                 res[n_spaces] = NULL;
-                if((pid = fork()) == 0)
+                if(foreground)
                 {
-                    execvp(tok,res); 
-                    exit(0);
+                    gettimeofday (&tvalBefore, NULL);
                 }
-                wait(&status);
+                if(SIGDET)
+                {   
+                    memset(&sa, 0, sizeof(sa));
+                    sa.sa_handler = sandler;
+                    //if(sigaction(SIGCHLD,&sa,0))
+                    //{
+                        //perror("sigaction");
+                        //return 1;
+                    //}
+                    if(!foreground)signal(SIGCHLD,sandler);
+                    signal(SIGINT,intHandler);
+                    pid = fork();
+                    if(pid == -1)
+                    {
+                        perror("Fork failed");
+                        exit(1);
+                    }
+                    else if(pid == 0)
+                    {   
+                        int exitStatus = 0;
+                        if(execvp(res[0],res)==-1)
+                        {
+                            exitStatus = 1;
+                        }
+                        exit(exitStatus);
+                    }
+                    else if(foreground)
+                    {   
+                        waitpid(pid,&status,0);
+                    }
+                }
+                else
+                {
+                    pid = fork();
+                    if(pid == -1)
+                    {
+                        perror("Fork failed");
+                        exit(1);
+                    }
+                    else if(pid == 0)
+                    {   
+                        int exitStatus = 0;
+                        if(execvp(res[0],res)==-1)
+                        {
+                            exitStatus = 1;
+                        }
+                        exit(exitStatus);
+                    }
+                    else if(foreground)
+                    {   
+                        waitpid(pid,&status,0);
+                    }
+                }
+                if(foreground)
+                {
+                    gettimeofday (&tvalAfter, NULL);
+                    printf("Time in microseconds: %ld microseconds\n",((tvalAfter.tv_sec - tvalBefore.tv_sec)*1000000L+tvalAfter.tv_usec) - tvalBefore.tv_usec);
+                }
+                free(res);
+                foreground = 0;
                 tok = strtok(NULL, delims);
             }
         }
-        stop = clock();
-        printf("%6.3f", start);
-        printf("\n\n%6.3f", stop);
-
-        printf("Execution took: %d\n",stop-start);
     }   
 }
